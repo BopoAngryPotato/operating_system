6de17d6e8cb397fe55737894c87a813f94dd5f60
pozhang <pzhang954@gmail.com>
2021-04-12 23:08:09 -0700

    Allocator status

--- ch9/project/allocator.cpp
+++ ch9/project/allocator.cpp
@@ -1,10 +1,15 @@
 #include <iostream>
 #include "allocator.h"
 
+#define STATUS "STAT"
 #define EXIT "X"
 #define SPACE ' '
 
 using namespace std;
+
+Allocator::Allocator(unsigned int space): max(space){
+  record.push_back({0, max-1, "", false});
+}
 
 void Allocator::run(){
   string buf;
@@ -12,9 +17,14 @@ void Allocator::run(){
     cout << "allocator>";
     getline(cin, buf);
     vector<string> params = parseCmd(buf);
-    for(size_t idx = 0; idx < params.size(); idx++)
-      cout << params[idx] << ' ';
-    cout << '\n';
+    while(!params.empty()){
+      if(params[0].empty()) params.erase(params.begin());
+      else break;
+    }
+    if(params.empty()) continue;
+    if(params[0] == STATUS) status(params);
+    else if(params[0] == EXIT) continue;
+    else cout << "Invalid command\n";
   }
 }
 
@@ -29,4 +39,23 @@ vector<string> Allocator::parseCmd(const string& cmd){
   }
   ret.push_back(cmd.substr(pos));
   return ret;
+}
+
+void Allocator::status(const vector<string>& params) const{
+  for(list<Block>::const_iterator itr = record.begin(); itr != record.end(); itr++){
+    cout << "Addresses [" << itr->first << ":" << itr->last << "] ";
+    if(itr->used) cout << "Process " << itr->process;
+    else cout << "Unused";
+    cout << "\n";
+  }
+}
+
+Allocator::Block::Block(): first(0), last(0), used(false){}
+
+Allocator::Block::Block(unsigned int pfirst, unsigned int plast, string pprocess = "", bool pused = false): first(pfirst), last(plast), process(pprocess), used(pused){
+  if(first > last) throw runtime_error("Error");
+}
+
+Allocator::Block::Block(const Block& blk): first(blk.first), last(blk.last), process(blk.process), used(blk.used){
+  if(first > last) throw runtime_error("Error");
 }
--- ch9/project/allocator.h
+++ ch9/project/allocator.h
@@ -4,23 +4,21 @@
 
 class Allocator{
   public:
-    Allocator(unsigned int space): max(space){};
+    Allocator(unsigned int space);
     void run();
   private:
     class Block{
       public:
-        Block():first(0), last(0), used(false){};
-        unsigned int getFirst() const{ return first; };
-        unsigned int getLast() const{ return last; };
-        std::string getProcess() const{ return process; };
-        bool getUsed() const{ return used; };
-      private:
+        Block();
+        Block(unsigned int, unsigned int, std::string, bool);
+        Block(const Block&);
         unsigned int first, last;
         std::string process;
         bool used;
     };
 
     std::vector<std::string> parseCmd(const std::string&);
+    void status(const std::vector<std::string>&) const;
 
     unsigned int max;
     std::list<Block> record;
